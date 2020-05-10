c3148a0ec36cd7d8ba6668a7981f54d888fc171b
seqre <seqre@disroot.org>
2020-05-10 13:03:25 +0200

    Add initial project files
    Signed-off-by: seqre <seqre@disroot.org>

---
+++ src/main/java/pl/uj/io/cuteanimals/CuteanimalsApplication.java
@@ -0,0 +1,13 @@
+package pl.uj.io.cuteanimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuteanimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuteanimalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/uj/io/cuteanimals/CuteanimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uj.io.cuteanimals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuteanimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
