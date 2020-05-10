82fa344be20cb335c703c94a9fad536b710c33f1
Lukasz Sereda <lukasz.sereda98@gmail.com>
2020-05-10 14:28:20 +0200

    change main class name

--- src/main/java/pl/uj/io/cuteanimals/CuteAnimalsApplication.java
+++ src/main/java/pl/uj/io/cuteanimals/CuteAnimalsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CuteanimalsApplication {
+public class CuteAnimalsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CuteanimalsApplication.class, args);
+		SpringApplication.run(CuteAnimalsApplication.class, args);
 	}
 
 }
