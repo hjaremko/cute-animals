7fe1864b29277de0cff9954a457687b512728aaa
Hubert Jaremko <hjaremko@outlook.com>
2020-05-18 00:36:55 +0200

    Add Google formatter and verify formatting (#2, #18)

--- src/main/java/pl/uj/io/cuteanimals/CuteAnimalsApplication.java
+++ src/main/java/pl/uj/io/cuteanimals/CuteAnimalsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CuteAnimalsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CuteAnimalsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CuteAnimalsApplication.class, args);
+    }
 }
--- src/test/java/pl/uj/io/cuteanimals/CuteAnimalsApplicationTests.java
+++ src/test/java/pl/uj/io/cuteanimals/CuteAnimalsApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CuteAnimalsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
