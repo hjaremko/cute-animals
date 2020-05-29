f19c2aec593a1d75e908a5675e1accd1cc44eff7
Klaudia <klaudia.goska06@gmail.com>
2020-05-29 12:25:42 +0200

    update

--- src/main/java/pl/uj/io/cuteanimals/model/Item.java
+++ src/main/java/pl/uj/io/cuteanimals/model/Item.java
@@ -1,13 +1,16 @@
 package pl.uj.io.cuteanimals.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import pl.uj.io.cuteanimals.model.interfaces.IAttributes;
 import pl.uj.io.cuteanimals.model.interfaces.IItem;
 
+/**
+ * Provides methods to represent items table.
+ *
+ * @version %I%
+ * @since 0.0.1-SNAPSHOT
+ */
 @Entity(name = "items")
 public class Item implements IItem {
     @JsonProperty("id")
@@ -18,19 +21,62 @@ public class Item implements IItem {
     @JsonProperty("name")
     private final String name;
 
+    @JsonProperty("description")
+    private final String description;
+
+    @JsonProperty("size")
+    private final int size;
+
+    @JsonProperty("attributes")
+    @ManyToOne
+    private final Attributes attributes;
+
     public Item() {
         this.id = 0;
         this.name = null;
+        this.description = null;
+        this.size = 0;
+        this.attributes = null;
     }
 
-    public Item(@JsonProperty("id") int id, @JsonProperty("name") String name) {
+    public Item(
+            @JsonProperty("id") int id,
+            @JsonProperty("name") String name,
+            @JsonProperty("description") String description,
+            @JsonProperty("size") int size,
+            @JsonProperty("attributes") Attributes attributes) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.size = size;
+        this.attributes = attributes;
     }
 
     @Override
     public String toString() {
-        return "Item {" + "name='" + name + "'" + ", id=" + id + "}";
+
+        return "Item {"
+                + "id="
+                + id
+                + ", name='"
+                + name
+                + "'"
+                + ", description='"
+                + description
+                + "'"
+                + ", size="
+                + size
+                + ", attributes="
+                + "{health="
+                + getAttributes().getHealth()
+                + ", attack="
+                + getAttributes().getAttack()
+                + ", level="
+                + getAttributes().getLevel()
+                + ", defence="
+                + getAttributes().getDefence()
+                + "}"
+                + "}";
     }
 
     @Override
@@ -40,16 +86,40 @@ public class Item implements IItem {
 
     @Override
     public String getDescription() {
-        return null;
+        return description;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
     @Override
     public IAttributes getAttributes() {
+
+        if (attributes != null) {
+            return new IAttributes() {
+                @Override
+                public int getHealth() {
+                    return attributes.getHealth();
+                }
+
+                @Override
+                public int getAttack() {
+                    return attributes.getAttack();
+                }
+
+                @Override
+                public int getLevel() {
+                    return attributes.getLevel();
+                }
+
+                @Override
+                public int getDefence() {
+                    return attributes.getDefence();
+                }
+            };
+        }
         return null;
     }
 }
